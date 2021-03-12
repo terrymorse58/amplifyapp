d7269a90e1bf816f3ae621636082dce023d23426
Terry Morse <tmorse@terrymorse.com>
2021-03-11 18:03:46 -0800

    version 0.1.2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          "Hello World" version 0.1.1
+          "Hello World" version 0.1.2
         </p>
         <a
           className="App-link"
