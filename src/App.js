b68cfa1a09148e0af2c2de9b325172e8af7072c9
Terry Morse <tmorse@terrymorse.com>
2021-03-11 18:25:31 -0800

    version 0.1.3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo-lisa.png';
+import logo from './Test-Logo.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          "Hello World" version 0.1.2
+          "Hello World" version 0.1.3
         </p>
         <a
           className="App-link"
